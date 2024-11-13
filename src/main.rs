2f656ee233cfd4c37de52bb9d060663ae7f3bf8d
SachinMaharana <sachin.nicky@gmail.com>
2024-11-13 15:20:24 +0100

    3.3

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,15 @@
-fn main() {
-    println!("Hello, world!");
+use actix_web::{web, App, HttpRequest, HttpResponse, HttpServer, Responder};
+#[tokio::main]
+async fn main() -> Result<(), std::io::Error>{
+    HttpServer::new(|| {
+        App::new()
+            .route("/health", web::get().to(health_check))
+
+    }).bind("127.0.0.1:8000")?
+        .run()
+        .await
+}
+
+async fn health_check(_req: HttpRequest) -> impl Responder {
+    HttpResponse::Ok().finish()
 }
